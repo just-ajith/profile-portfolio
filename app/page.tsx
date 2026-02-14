a49b9f497f7cc5abe17c84b2908086d7ed217027
just-ajith <ajsmart1161997@gmail.com>
2026-02-14 20:06:36 +0530

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      This is Ajith self thambattam
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:white">
+      <div className="text-blue-900">
+        This is Ajith self thambattam
+
+      </div>
     </div>
   );
 }
